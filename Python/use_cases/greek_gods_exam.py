e21813037514cb825822c15c6f4a1dbf606a74a6
Gautam Andani <andaniji2@gmail.com>
2024-04-11 03:32:59 +0530

    greek gods 3
    .combine_first() combines 1st column with second column , fills with first column,
    if NAN or error value fill with second column

--- Python/use_cases/greek_gods_exam.py
+++ Python/use_cases/greek_gods_exam.py
@@ -11,8 +11,8 @@ df2 = pd.read_csv(godesses)
 df_merged = pd.merge(df1, df2, on=['Domain', 'Symbol', 'Age'], how='outer')
 # print(df_merged.head(11))
 
-df1.rename(columns={'God':'God/Goddess'}, inplace=True)
-df2.rename(columns={'Goddess':'God/Goddess'}, inplace=True)
+# df1.rename(columns={'God':'God/Goddess'}, inplace=True)
+# df2.rename(columns={'Goddess':'God/Goddess'}, inplace=True)
 
 df3 = pd.concat([df1, df2], axis=0)     # joins multiple df along rows or columns , if you don't rename it'll be same as df_merged
 # print(df3.head(10))
@@ -21,15 +21,21 @@ df3 = pd.concat([df1, df2], axis=0)     # joins multiple df along rows or column
 
 df4_1 = df3[df3['Age']>8000].sort_values(by='Age', ascending=False)
 df4_2 = df3.query('Age>8000').sort_values(by='Age', ascending=False) 
-print(df4_1)
+# print(df4_1)
 
 class TestClass(unittest.TestCase):
     def test_shape_verify(self):        # the test method name must start with test for test to run
         self.assertEqual(df4_1.shape, df4_2.shape)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
 
 ############### 3
 
+df5 = pd.merge(df1, df2, on='Domain', how='outer')
+df5 = df5.groupby('Domain').mean('Age').reset_index()
+df5['Age'] = df5['Age_x'].combine_first(df5['Age_y'])   # fills column with 1st column if nan fills with second column
+print(df5[['Domain','Age']])       # since domain is index it'll come
+
+########## 4
