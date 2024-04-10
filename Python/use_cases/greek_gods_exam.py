ab762976e8105ed01a3f6a3237fdfe961e13e4ee
Gautam Andani <andaniji2@gmail.com>
2024-04-11 01:50:41 +0530

    Unit test method must start with test in it's name

--- Python/use_cases/greek_gods_exam.py
+++ Python/use_cases/greek_gods_exam.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import unittest
 
 ################### 1
 gods = "dataset/greek_gods.csv"
@@ -14,14 +15,21 @@ df1.rename(columns={'God':'God/Goddess'}, inplace=True)
 df2.rename(columns={'Goddess':'God/Goddess'}, inplace=True)
 
 df3 = pd.concat([df1, df2], axis=0)     # joins multiple df along rows or columns , if you don't rename it'll be same as df_merged
-print(df3.head(10))
+# print(df3.head(10))
 
 ################ 2
 
-df4 = df3[df3['Age']>8000].sort_values(by='Age', ascending=False)
-df4 = df3.query('Age>8000').sort_values(by='Age', ascending=False) 
-print(df4)
+df4_1 = df3[df3['Age']>8000].sort_values(by='Age', ascending=False)
+df4_2 = df3.query('Age>8000').sort_values(by='Age', ascending=False) 
+print(df4_1)
+
+class TestClass(unittest.TestCase):
+    def test_shape_verify(self):        # the test method name must start with test for test to run
+        self.assertEqual(df4_1.shape, df4_2.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 ############### 3
 
-##### do unit test on 1st 3
